347fd9884cf2ea88243673dd8643193375a1e65a
H <test@test.com>
2026-05-28 15:11:09 +0100

    added fastapi example

---
+++ my-api/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+
+# Write JSON line entries inside a list with some example books and their availability.
+
+app = FastAPI()
+
+# Fake DB
+books = [
+    {"id":1, "title":"Harry Potter", "available":True},
+    {"id":2, "title":"Eragon", "available":False},
+    {"id":3, "title":"The Hobbit", "available":True}                          
+]
+
+# Home Route
+@app.get("/")
+def home():
+    return {"message":"Haf's Library API"}
+            
+# Get All Book
+@app.get("/books")
+def get_book():
+    return books
+
+# import json
+
+# books = [
+#     {"title": "Avengers", "availability": "1 available" },
+#     {"title": "Superman", "availability": "4 available" },
+#     {"title": "Spiderman", "availability": "2 available" }
+# ]
+
+# with open("books.json1", "w", encoding="utf-8") as file:
+#     for book in books:
+#         json_line = json.dumps(book, ensure_ascii=False)
+#         file.write(json_line + "\n")
